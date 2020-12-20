3d059d0150596d032802e0ebb40302c37b5a8213
Emily Preuß <emilyalice.preuss@gmail.com>
2020-12-20 13:32:14 +0100

    Add load_data function

--- bikeshare.py
+++ bikeshare.py
@@ -78,3 +78,41 @@ def get_filters():
 
     print('-'*40)
     return city, month, day
+
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Parameters:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+    #load data into dataframe
+    print("Loading your selected data set...\n")
+    df = pd.read_csv(CITY_DATA[city])
+
+    #convert the Start Time column to datetime
+    df["Start Time"] = pd.to_datetime(df["Start Time"])
+
+    # extract month and day of week from Start Time to creat new columns
+    df["month"] = df["Start Time"].dt.month
+    df["day_of_week"] = df["Start Time"].dt.day_name()
+
+     # filter by month if applicable
+    if month != "all":
+        #user the index of the months list to get the corresponding int
+        months=["january", "february", "march", "april", "may", "june"]
+        month = months.index(month) + 1
+
+        #filter by month to create the new dataframe
+        df = df[df["month"] == month]
+
+    # filter by month if applicable
+    if day != "all":
+        #filter by day to get the new dataframe
+         df = df[df['day_of_week'] == day.title()]
+
+    return df
